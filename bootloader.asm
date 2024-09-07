94f6e9b7dc9521950e4f1490b5840191eea04128
Ignacio <ignaciograner@gmail.com>
2024-09-07 14:04:49 -0600

    Add some constants

--- bootloader.asm
+++ bootloader.asm
@@ -1,18 +1,27 @@
 [BITS 16]            ; Code for 16-bit mode
 [ORG 0x7C00]         ; Bootloader load address
 
+;; CONSTANTS ===================================================
+SCREEN_WIDTH       equ 320  ; Width in pixels
+SCREEN_HEIGHT      equ 200  ; Height in pixels
+
+BLUE_COLOR         equ 0x01 ; Color blue
+
+
 start:
+    ;; SETUP ===================================================
     mov ax, 0x0013   ; Set video mode to 320x200 with 256 colors
-    int 0x10         ; Call video interrupt
+    int 0x10         ; Call video interrupt for video services
+    ;; =========================================================
 
     mov ah, 0x0C     ; Function to set a pixel
-    mov al, 0x01     ; Blue color (commonly in default palette)
+    mov al, BLUE_COLOR     ; Blue color (commonly in default palette)
     xor cx, cx       ; X coordinate = 0
     xor dx, dx       ; Y coordinate = 0
     int 0x10         ; Call video interrupt to set the pixel
 
     ; Paint the entire screen blue
-    mov cx, 320*200  ; Total pixels to paint
+    mov cx, SCREEN_WIDTH*SCREEN_HEIGHT  ; Total pixels to paint
     mov bx, 0         ; Initial pixel position 
     loop_segment:
         mov es, ax   ; Set video segment (0xA000 for mode 13h)
@@ -27,5 +36,5 @@ start:
     jmp hang         ; Infinite loop to stop the bootloader
 
     times 510 - ($ - start) db 0   ; Fill the rest of the sector zeros 
-    dw 0xAA55                       ; Boot sector signature
+    dw 0xAA55                      ; Boot sector signature
 
