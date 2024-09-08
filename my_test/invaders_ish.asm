f4629db39ea935f7a98446dd2a93cf1a045535e6
Ignacio <ignaciograner@gmail.com>
2024-09-08 12:42:13 -0600

    Semi-drawing

--- my_test/invaders_ish.asm
+++ my_test/invaders_ish.asm
@@ -15,15 +15,13 @@ letter3      equ 0FA04h
 letter4      equ 0FA06h
 letter5      equ 0FA08h
 letter6      equ 0FA0Ah
-;barrierArr   equ 0FA0Ch
+;letter7      equ 0FA0Ch
+
 alienArr     equ 0FA20h  ; 2 words (1 dblword) - 32bits/aliens
-;playerX      equ 0FA24h
-;shotsArr     equ 0FA25h  ; 4 Y/X shot values - 8 bytes, 1st shot is player
-alienY       equ 0FA2Dh
-alienX       equ 0FA2Eh
-;num_aliens   equ 0FA2Fh  ; # of aliens still alive
-;direction    equ 0FA30h  ; # pixels that aliens move in X direction
-;move_timer   equ 0FA31h  ; 2 bytes (using BP) - # of game loops/timer ticks to wait before aliens move
+
+letterY       equ 0FA2Dh ; 1 byte
+letterX       equ 0FA2Eh ; 1 byte
+
 change_alien equ 0FA33h  ; Use alternate sprite yes/no
 
 ;; CONSTANTS =====================================
@@ -82,25 +80,17 @@ game_loop:
     rep stosb       ; mov [ES:DI], al cx # of times
 
     ;; ES:DI now points to AFA00h
-    ;; Draw aliens ------------------------------------------------
+    ;; Draw letters ------------------------------------------------
     mov si, alienArr
     mov bl, LETTER_COLOR
     mov ax, [si+13]       ; AL = alienY, AH = alienX
     mov cl, 4
     mov dl, byte[si+19]
-    ;cmp byte [si+19], cl  ; Change alien? CL = 0 from above
-    ;mov cl, 4
 
-    ;jg draw_next_alien_row    ; Nope, use normal sprite
-    ;add di, cx                ; Yes, use alternate sprite  (CX = 4)
 
-    ;cmp byte [si+19], 0
-    ;je draw_next_alien_row
-    ;cmp byte [si+19], 1
-    ;je second_sprite
 
     cmp dl, 0
-    je draw_next_alien_row
+    je draw_next_letter
 
     cmp dl, 1
     je sprite_1
@@ -111,30 +101,39 @@ game_loop:
     cmp dl, 3
     je sprite_3
 
-    ;add di, cx
-    jmp draw_next_alien_row
+    cmp dl, 4
+    je sprite_4
+
+    cmp dl, 5
+    je sprite_5
+
+    cmp dl, 6
+    je sprite_6
+
+
+    ;jmp draw_next_letter
 
     sprite_1:
         add di, cx
-        jmp draw_next_alien_row
+        jmp draw_next_letter
 
     sprite_2:
         add di, cx
         add di, cx
-        jmp draw_next_alien_row
+        jmp draw_next_letter
 
     sprite_3:
         add di, cx
         add di, cx
         add di, cx
-        jmp draw_next_alien_row
+        jmp draw_next_letter
 
     sprite_4:
         add di, cx
         add di, cx
         add di, cx
         add di, cx
-        jmp draw_next_alien_row
+        jmp draw_next_letter
 
     sprite_5:
         add di, cx
@@ -142,59 +141,57 @@ game_loop:
         add di, cx
         add di, cx
         add di, cx
-        jmp draw_next_alien_row
+        jmp draw_next_letter
 
-draw_next_alien_row:
+    sprite_6:
+        add di, cx
+        add di, cx
+        add di, cx
+        add di, cx
+        add di, cx
+        add di, cx
+        jmp draw_next_letter
+
+
+    draw_next_letter:
         pusha
-        mov cl, 13             ; # of aliens to check per row
-        check_next_alien:
+        mov cl, 13             ; # of letter to write in row
+        .check_next_letter:
             pusha
             dec cx
-            ;bt [si], cx     ; Bit test - copy bit to carry flag
-            ;jnc .next_alien ; Not set, skip
 
-            mov si, di      ; SI = alien sprite to draw
+
+            mov si, di
             call draw_sprite
 
-            .next_alien:
+            .next_letter:
                 popa
                 add ah, SPRITE_WIDTH+2
 
-        loop check_next_alien
+        loop .check_next_letter
 
         popa
-    loop draw_next_alien_row
-
-    ;; Draw player ship -------------------------------------------
-    ;; SI currently poins to playerX
-    ;lodsb       ; AL = playerX
-    ;push si
-    ;mov si, ship
-    ;mov ah, PLAYERY
-    ;xchg ah, al ; Swap playerX and playerY values
-    ;mov bl, PLAYER_COLOR
-    ;call draw_sprite
+    loop draw_next_letter
 
 
-    ;; Move aliens ------------------------------------------------
-    ;; TODO: Try to change to use leftmost/rightmost aliens for comparisons,
-    ;;   don't just check the top left alien
-    move_aliens:
+
+    ;; Move letters ------------------------------------------------
+    move_letters:
         ;; Using BP for move_timer, Push/pop only affects SP, BP is unaffected
-        mov di, alienX
+        mov di, letterX
 
         ;; Yes, move aliens
-        neg byte [change_alien]     ; Toggle change_alien byte between 1 & -1, use next sprite
+        ;neg byte [change_alien]     ; Toggle change_alien byte between 1 & -1, use next sprite
 	    mov al, byte [change_alien]
 	    inc al
-	    cmp al, 6
+	    cmp al, 7
 
 	    jl continue
         mov al, 0
-	
+
 	    continue:
 	        mov byte [change_alien], al
-        
+
 
 
 
@@ -210,15 +207,9 @@ jmp game_loop
 ;; END GAME LOOP =====================================
 
 
+
+
 ;; Draw a sprite to the screen
-;; Input parameters:
-;;   SI = address of sprite to draw
-;;   AL = Y value of sprite
-;;   AH = X value of sprite
-;;   BL = color
-;; Clobbers:
-;;   DX
-;;   DI
 draw_sprite:
     call get_screen_position    ; Get X/Y position in DI to draw at
     mov cl, SPRITE_HEIGHT
@@ -234,8 +225,8 @@ draw_sprite:
             cmovc ax, bx        ; Yes bit is set, move BX into AX (BL = color)
             mov ah, al          ; Copy color to fill out AX
             mov [di+SCREEN_WIDTH], ax
-            stosw                   
-        jnz .next_pixel                               
+            stosw
+        jnz .next_pixel
 
         add di, SCREEN_WIDTH*3-SPRITE_WIDTH_PIXELS
         pop cx
@@ -243,13 +234,9 @@ draw_sprite:
 
     ret
 
+
+
 ;; Get X/Y screen position in DI
-;; Input parameters:
-;;   AL = Y value
-;;   AH = X value
-;; Clobbers: 
-;;   DX
-;;   DI
 get_screen_position:
     mov dx, ax      ; Save Y/X values
     cbw             ; Convert byte to word - sign extend AL into AH, AH = 0 if AL < 128
@@ -260,68 +247,51 @@ get_screen_position:
 
     ret
 
+
+
 ;; CODE SEGMENT DATA =================================
 sprite_bitmaps:
-    db 1111b    ; Letter 0 bitmap
+    db 1111b    ; Letter 0 bitmap (I)
     db 0110b
     db 0110b
     db 1111b
 
-    db 1111b    ; Letter 1 bitmap
+    db 1111b    ; Letter 1 bitmap (G)
     db 1000b
     db 1111b
     db 1111b
 
-    db 1001b    ; Letter 2 bitmap
+    db 1001b    ; Letter 2 bitmap (N)
     db 1101b
     db 1011b
     db 1001b
 
-    db 1111b    ; Letter 3 bitmap
+    db 1111b    ; Letter 3 bitmap (A)
     db 1001b
     db 1111b
     db 1001b
 
-    db 1111b    ; Letter 4 bitmap
-    db 1001b
+    db 1111b    ; Letter 4 bitmap (C)
+    db 1000b
     db 1000b
     db 1111b
 
-    db 1111b    ; Letter 5 bitmap
+    db 1111b    ; Letter 5 bitmap (I)
     db 0110b
     db 0110b
     db 1111b
 
-    ;;; Rotate izq 90
-    db 1001b    ; Letter 0 bitmap
-    db 1111b
-    db 1111b
+    db 1111b    ; Letter 6 bitmap (O)
     db 1001b
+    db 1001b
+    db 1111b
 
 
 
 
-    db 11000011b    ; Player ship bitmap
-    db 11100011b
-    db 11011011b
-    db 11100111b
 
-    db 00111100b    ; Barrier bitmap
-    db 01111110b
-    db 11100111b
-    db 11100111b
 
-    ;; Initial variable values
-    dw 0FFFFh       ; Alien array
-    dw 0FFFFh
-    ;db 70           ; PlayerX
-    ;; times 8 db 0 ; Shots array
-    dw 230Ah        ; alienY & alien X | 10 = Y, 35 = X
-    db 20h          ; # of aliens = 32 TODO: Remove & check if alienArr = 0, 
-                    ;   This is probably not needed, can save some bytes
-    db 0FBh         ; Direction -5
-    dw 18           ; Move timer
-    db 1            ; Change alien - toggle between 1 & -1
+
 
 ;; Boot signature ===================================
 times 510-($-$$) db 0
